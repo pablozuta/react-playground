0f5510c5ae55a57a87594701be3182e4ae0ea7ee
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-15 14:27:13 -0400

    se agrega data json students y component 'Students.jsx'

---
+++ src/components/Students.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import studentData from '../datas/students.json'
+
+function Students() {
+    console.log(studentData);
+    return (
+        <div>Students</div>
+    )
+}
+
+export default Students
